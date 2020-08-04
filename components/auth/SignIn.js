90dbdf7f290ac8dd5c3db560a3a6c9f65bd1f994
pranav589 <pranav.bakale@gmail.com>
2020-08-04 19:23:30 +0530

    4 files modified

--- components/auth/SignIn.js
+++ components/auth/SignIn.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 
 class SignIn extends Component {
   state = {
-    
+    email:'',
+    password:''
   }
   handleChange = (e) => {
-    
+    this.setState({
+      [e.target.id]:e.target.value
+    })
   }
   handleSubmit = (e) => {
-    
+    e.preventDefault();
+    console.log(this.state)
     
   }
   render() {
