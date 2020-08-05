b8c254435e600b787cb1eff9d09d1bde1e852122
pranav589 <pranav.bakale@gmail.com>
2020-08-05 20:04:56 +0530

    4 files modified

--- store/reducers/projectReducer.js
+++ store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState={}
+const initState={
+  projects:[
+    {id:'1',title:'help me ',content:'bla bla bla'},
+    {id:'2',title:'help me out',content:'bla bla bla'},
+    {id:'3',title:'help me plz',content:'bla bla bla'}
+  ]
+}
 
 const projectReducer=(state=initState,action)=>{
   return state;
